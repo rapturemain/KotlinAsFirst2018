ec70f89369babde113b15449c706ee4d1020aaec
rapturemain <rapture.main@gmail.com>
2018-11-05 23:27:10 +0300

    Lesson 7

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -265,7 +265,8 @@ fun top20Words(inputName: String): Map<String, Int> {
             map[it] = map.getOrDefault(it, 0) + 1
         }
     }
-    map = map.map { it.key to it.value }.reversed().toMap().toMutableMap().toSortedMap()
+    map = map.map { it.key to it.value }.sortedBy { it.second }
+            .reversed().toMap().toMutableMap()
     return if (map.size <= 20) map
     else {
         val buffer = mutableMapOf<String, Int>()
