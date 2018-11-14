23a84d2f36fd1b510c81b3daa2369f19a8d29949
rapturemain <rapture.main@gmail.com>
2018-11-14 22:09:13 +0300

    Lesson 7

--- test/lesson7/task1/Tests.kt
+++ test/lesson7/task1/Tests.kt
@@ -286,6 +286,60 @@ Basic, Ruby, Swift.
         checkHtmlListsExample()
     }
 
+    private fun checkHtmlListsExample2() {
+        val result = File("temp.html").readText().replace(Regex("[\\s\\n\\t]"), "")
+        val expected =
+                """
+                    <html>
+                      <body>
+                      <p>
+                        <ul>
+                          <li>
+                            Утка по-пекински
+                            <ul>
+                              <li>Утка</li>
+                              <li>Соус</li>
+                            </ul>
+                          </li>
+                          <li>
+                            Салат Оливье
+                            <ol>
+                              <li>Мясо
+                                <ul>
+                                  <li>
+                                      Или колбаса
+                                  </li>
+                                </ul>
+                              </li>
+                              <li>Майонез</li>
+                              <li>Картофель</li>
+                              <li>Что-то там ещё</li>
+                            </ol>
+                          </li>
+                          <li>Помидоры</li>
+                          <li>
+                            Фрукты
+                            <ol>
+                              <li>Бананы</li>
+                              <li>
+                                Яблоки
+                                <ol>
+                                  <li>Красные</li>
+                                  <li>Зелёные</li>
+                                </ol>
+                              </li>
+                            </ol>
+                          </li>
+                        </ul>
+                        </p>
+                      </body>
+                    </html>
+                    """.trimIndent().replace(Regex("[\\s\\n\\t]"), "")
+        assertEquals(expected, result)
+
+        File("temp.html").delete()
+    }
+
     @Test
     @Tag("Impossible")
     fun markdownToHtml() {
@@ -293,7 +347,7 @@ Basic, Ruby, Swift.
         checkHtmlSimpleExample()
 
         markdownToHtml("input/markdown_lists.md", "temp.html")
-        checkHtmlListsExample()
+        checkHtmlListsExample2()
     }
 
     @Test
